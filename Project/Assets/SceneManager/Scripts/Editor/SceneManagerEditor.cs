38f1cf393d1ee5167b2902e419df8df46a5a1d47
Okamochi000 <okkanw@yahoo.co.jp>
2021-09-05 20:52:11 +0900

    namespaceを外す

--- Project/Assets/SceneManager/Scripts/Editor/SceneManagerEditor.cs
+++ Project/Assets/SceneManager/Scripts/Editor/SceneManagerEditor.cs
@@ -1,20 +1,17 @@
 using UnityEditor;
 
-namespace Utility
+[CustomEditor(typeof(SceneManager))]
+public class SceneManagerEditor : CommonEditor
 {
-    [CustomEditor(typeof(SceneManager))]
-    public class SceneManagerEditor : CommonEditor
-    {
-        private bool isOpen_ = false;
+    private bool isOpen_ = false;
 
-        /// <summary>
-        /// 継承先の更新処理
-        /// </summary>
-        protected override void OnInspectorGUIBody()
-        {
-            ShowProperty("isScenePrefab");
-            if (GetProperty("isScenePrefab").boolValue) { ShowEnumListLayout(typeof(SceneType), "scenePrefabList", ref isOpen_); }
-            else { ShowEnumListLayout(typeof(SceneType), "sceneNameList", ref isOpen_); }
-        }
+    /// <summary>
+    /// 継承先の更新処理
+    /// </summary>
+    protected override void OnInspectorGUIBody()
+    {
+        ShowProperty("isScenePrefab");
+        if (GetProperty("isScenePrefab").boolValue) { ShowEnumListLayout(typeof(SceneType), "scenePrefabList", ref isOpen_); }
+        else { ShowEnumListLayout(typeof(SceneType), "sceneNameList", ref isOpen_); }
     }
 }
